dc2d689bd3cace0657e9a0c8e29f1a78f595ed7f
GeethapriyaVignesh <geethasenthilcse@gmail.com>
2023-07-27 11:45:40 +0530

    add

--- App.js
+++ App.js
@@ -73,7 +73,7 @@ export default function Game() {
     if (move > 0) {
       description = 'Go to move #' + move;
     } else {
-      description = 'Go to game start';
+      description = 'GAME START';
     }
     return (
       <li key={move}>
